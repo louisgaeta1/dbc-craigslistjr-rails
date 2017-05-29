f3ddaeb2f3047bbbac2403c957cbe9b9a0853bec
Lou Gaeta <louis.gaeta1@gmail.com>
2017-05-29 14:44:19 -0400

    Correction: changes before_save to before_create

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :category
-  before_save :generate_key
+  before_create :generate_key
 
   def generate_key
     self.key = [*'a'..'z',*'A'..'Z',*'0'..'9'].sample(8).join("")
   end
-  
+
 end

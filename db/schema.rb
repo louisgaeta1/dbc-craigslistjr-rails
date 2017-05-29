e21c9119e3a81cd615d0140d3207bf71bd169433
Lou Gaeta <louis.gaeta1@gmail.com>
2017-05-29 14:42:38 -0400

    Migrates Article table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529175834) do
+ActiveRecord::Schema.define(version: 20170529183918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.string "email", null: false
+    t.decimal "price"
+    t.bigint "category_id"
+    t.string "key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +33,5 @@ ActiveRecord::Schema.define(version: 20170529175834) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "categories"
 end
